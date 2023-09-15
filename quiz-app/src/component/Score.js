11f371fac0025e234a215c247c07165609f07244
Nwpatt <nawapat_link@hotmail.com>
2023-09-15 15:01:59 +0700

    update score.js

--- quiz-app/src/component/Score.js
+++ quiz-app/src/component/Score.js
@@ -3,11 +3,17 @@ import { DataContext } from "../App";
 import QuestionsData from "../data/QuestionsData";
 
 const Score = ()=>{
-    const {score} = useContext(DataContext)
+    const {score,setAppState,setScore} = useContext(DataContext)
+
+    const restartApp=()=>{
+        setScore(0)
+        setAppState("menu")
+    }
     return(
         <div className="score">
             <h1>สรุปผลคะแนน</h1>
             <h2>{score} / {QuestionsData.length}</h2>
+            <button onClick={restartApp}>ทำแบบทดสอบอีกครั้ง</button>
         </div>
     )
 }

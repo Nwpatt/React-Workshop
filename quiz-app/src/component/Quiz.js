0beb7fc94d7b39e6b54aafab0d1e7ab43ec41c2f
Nwpatt <nawapat_link@hotmail.com>
2023-09-15 14:32:23 +0700

    Update Quiz.js

--- quiz-app/src/component/Quiz.js
+++ quiz-app/src/component/Quiz.js
@@ -4,6 +4,7 @@ import QuestionsData from "../data/QuestionsData";
 const Quiz = ()=>{
     // console.log(QuestionsData);
     const [current, setCurrent] = useState(0)
+    const [selectChoice,setSelectChoice] = useState("")
     return(
         <div className="quiz">
             <h1>{QuestionsData[current].question}</h1>
